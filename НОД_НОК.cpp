f15f46233e4fc9bc7f8cba14a5b370b75af69c7f
Pervuhin Roman <rg.pervuhin5198@gmail.com>
2022-02-10 10:57:25 +0500

    Update НОД_НОК.cpp

--- НОД_НОК.cpp
+++ НОД_НОК.cpp
@@ -1,9 +1,6 @@
 //https://brestprog.by/topics/gcd/
 
 //Длинный НОД (долго выполняется)
-
-#include <iostream>
-using namespace std;
 int gcd(int a, int b) {
     while(a != b) {
         if(a > b) {
@@ -15,17 +12,8 @@ int gcd(int a, int b) {
     
     return a;
 }
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    cout << gcd(a, b);
-    return 0;
-}*/
 
 //Короткий НОД (самый лучший вариант т.к. по времени самый быстрый)
-#include <iostream>
-using namespace std;
 int gcd(int a, int b) {
     while(a != 0 and b != 0){
         if(a < b)
@@ -36,32 +24,13 @@ int gcd(int a, int b) {
     
     return a + b;
 }
-int main() {
-    int a, b;
-    cin >> a >> b;
-    
-    cout << gcd(a, b);
-}
-*/
 
 /*Рекурсивный НОД (самы короткий по размеру программы, 
 но медленный по сравнению с предыдущем, идеален, когда необходимо быстро реализовать НОД)*/
-
-#include <iostream>
-using namespace std;
 int gcd(int a, int b) {
     if(b == 0)
         return a;
     return gcd(b ,a % b);
-}
-int lcm(int a, int b) {
-    return (a / gcd(a, b)) * b;
-}
-int main() {
-    int a, b;
-    cin >> a >> b;
-    
-    cout << gcd(a, b);
 }
 
 //Алгоритм нахождения НОК
